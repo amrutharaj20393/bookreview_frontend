facee59a5f24ed5d6e29b43f96e1bcbc4b702eec
amrutharaj20393 <amrutharaj20393@gmail.com>
2025-08-14 12:03:07 +0530

    url

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,3 +1,3 @@
-export const serverUrl='http://localhost:3000'
+//export const serverUrl='http://localhost:3000'
 
-//export const serverUrl='https://bookreview-backend-agu7.onrender.com'
+export const serverUrl='https://bookreview-backend-agu7.onrender.com'
